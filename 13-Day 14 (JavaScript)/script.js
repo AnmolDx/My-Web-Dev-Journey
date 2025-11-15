ed02063774f3a9c6aaa3ffa8d7f48a86fd7d2683
Anmol <129307316+AnmolDx@users.noreply.github.com>
2025-11-15 21:59:30 +0530

    day 14 completed
    today i started javascript

---
+++ 13-Day 14 (JavaScript)/script.js
@@ -0,0 +1,47 @@
+alert("hello world")
+// this will display message whenever user refresh
+
+console.log("i am console")
+console.log("console is running")
+console.log("console is also running")
+// used to check execution and debugging in the code 
+
+var num= prompt("enter a number:")//takes input from user
+
+var tr= confirm("are you sure to give me access to your computer")
+//takes yes or no as input from user
+
+console.log("your number is " + a)// this helps to show user input in console
+
+document.title ="JS tutorial" //change the title of html document
+
+var a = 6; //number
+var b = 9;
+var c = "Anmol"; //string
+//var 69t = 55 
+//not allowed
+console.log( a + b + 1)
+console.log(typeof a, typeof b, typeof c) //to get datatype, we can use type of
+
+// const a1 = 88;
+// a1 = a1 + 1;
+// not allowed because const value cannot be changed after assigned
+
+let p = "its me";
+let q = 98;
+let r = 2.6;
+const s = true;
+let t = null;
+let u = undefined;
+console.log(p, q, r, s, t, u)
+console.log(typeof p,typeof q,typeof r,typeof s,typeof t,typeof u)
+
+// we can make object by 
+let o = {
+   "name": "Anmol",
+   "roll no": 69
+}
+console.log(o)
+// to add something or change something we use
+o.degree = "BCA"
+console.log (o)
