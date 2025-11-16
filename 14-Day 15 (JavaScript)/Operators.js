725e09fb186a96eb704bdb09a213a864775a6a71
Anmol <129307316+AnmolDx@users.noreply.github.com>
2025-11-16 23:53:08 +0530

    day 15 not completed yet

---
+++ 14-Day 15 (JavaScript)/Operators.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b = 5;
+
+//Arithmatic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Unary Operators
+console.log(a++); //it only adds the value and store it
+console.log(a);
+console.log(++a); // it adds and show
+
+//Assignment Operators
+console.log(a+=b); //a = a + b
+console.log(a-=b); //a = a - b
+console.log(a*=b); //a = a * b
+console.log(a/=b); //a = a / b
+console.log(a%=b); //a = a % b
+
+//Comparision Operators
+console.log(a>b);  //Greater than
+console.log(a>=b); //greater than equal to
+console.log(a<b); //less than
+console.log(a<=b); //less than equal to
+console.log(a==b); //equal to (compares value, not type)
+console.log(a==="b"); //equal to (compares both type and value)
+console.log(a!=b); //not equal to
---
+++ 14-Day 15 (JavaScript)/console.log.js
@@ -0,0 +1,8 @@
+console.log ("hello", "world", 22, (5+5)) //we can print multiple values 
+
+let a = 10;
+let b = 5;
+// let sum = "the sum of a and b is" + (a + b) + "please check it";
+let sum =  `the sum of a and b is ${a + b} please check it` 
+//this is template literals form it eliminate the hassale of writing quotes 
+console.log(sum)
