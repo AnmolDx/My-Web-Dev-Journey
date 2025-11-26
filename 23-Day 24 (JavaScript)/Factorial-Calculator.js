e780733ff1ac9c3bb64086ed70da60e6314acc25
Anmol <129307316+AnmolDx@users.noreply.github.com>
2025-11-26 22:39:15 +0530

    Add files via upload

---
+++ 23-Day 24 (JavaScript)/Factorial-Calculator.js
@@ -0,0 +1,13 @@
+// create a factorial calculator using for loop and reduce method
+
+//factorial calculator using for loop
+let num = 5
+let fact = 1
+for (let i = 1; i<=num; i++){
+   fact = fact*i
+}
+console.log(fact)
+
+//factorial calculator using reduce method
+
+console.log(num.reduce())
