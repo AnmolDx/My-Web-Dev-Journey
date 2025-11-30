700d34673ff0b10e758a4b3621ce5f98272012d7
Anmol <129307316+AnmolDx@users.noreply.github.com>
2025-11-30 14:17:34 +0530

    Add actual answer
    Refactor color assignment to use a function for random colors.

--- 25-Day 26 (JavaScript)/Color-the-box.js
+++ 25-Day 26 (JavaScript)/Color-the-box.js
@@ -1,9 +1,24 @@
-let r = Math.floor(Math.random()*255)
-let g = Math.floor(Math.random()*255)
-let b = Math.floor(Math.random()*255)
+// let r = Math.floor(Math.random()*255)
+// let g = Math.floor(Math.random()*255)
+// let b = Math.floor(Math.random()*255)
+
+// let boxes = document.querySelectorAll(".box")
+// boxes.forEach(box => {
+//    box.style.backgroundColor = `rgb(${r},${g},${b})`
+//    box.style.color = `rgb(${b},${r},${g})`
+
+// });
+
+//Actual answer
+function getRandomColor() {
+   let val1 = Math.floor(Math.random()*255)
+   let val2 = Math.floor(Math.random()*255)
+   let val3 = Math.floor(Math.random()*255)
+   return `rgb(${val1},${val2},${val3})`
+}
 
 let boxes = document.querySelectorAll(".box")
-boxes.forEach(box => {
-   box.style.backgroundColor = `rgb(${r},${g},${b})`
-   box.style.color = `rgb(${b},${r},${g})`
-});
+Array.from(boxes).forEach(e => {
+   e.style.backgroundColor = getRandomColor()
+   e.style.color = getRandomColor()
+})
