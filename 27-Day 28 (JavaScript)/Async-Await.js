922a31b080b26415c3c7d61b14f97627936e2802
Anmol <129307316+AnmolDx@users.noreply.github.com>
2025-12-02 23:19:28 +0530

    day 27 still remaining

---
+++ 27-Day 28 (JavaScript)/Async-Await.js
@@ -0,0 +1,20 @@
+async function getData() { //async will help in running the function in background
+   return new Promise((resolve, reject)=>{
+      setTimeout(() => {
+         resolve(455)
+      }, 3500);
+   })
+}
+
+async function main() {
+   console.log("loading modules");
+   console.log("Do something else");
+   console.log("load data");
+   let data = await getData()
+   console.log(data);
+   console.log("task2")
+   
+   console.log("process data");
+}
+
+main()
